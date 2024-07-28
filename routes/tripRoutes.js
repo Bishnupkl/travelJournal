2fa46376a7dc4f9d8eb39dace5873b335d9a6170
bishnu <bishnup212@gmail.com>
2024-07-28 09:33:33 -0500

    changing the routes and controller

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -8,6 +8,7 @@ router.get('/', tripController.getAllTrips);
 router.get('/:id', tripController.getOneTrip);
 router.post('/', tripController.createTrip);
 router.delete('/:id', tripController.deleteTrip);
+router.put('/:id', tripController.updateTrip);
 router.get('/:id/gallery', tripController.getTripGallery);
 router.post('/:id/gallery', tripController.addGalleryEntry);
 router.get('/:tripId/gallery/:entryId', tripController.getGalleryEntry);
