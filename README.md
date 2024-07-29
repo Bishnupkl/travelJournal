65d05171e3e463698cd99f755cdae532e2aeace4
Bishnu pokhrel <43365095+Bishnupkl@users.noreply.github.com>
2024-07-28 19:56:05 -0500

    Bishnu (#4)
    * changing the callbackify function
    
    * changing the routes and controller
    
    * Updating readme.md
    
    * changing the folder structure for model
    
    * updating the patch routes

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Project Title is Travel Journal
 I have made main document : Trips with attributes country ,airport and hotel
 Sub document : Gallery with attribute place and picture.
 
+---------------------------------------------------------------------------------
+CRUD database (json file) is inside data folder.
+
+
--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -1,18 +1,27 @@
-
-
+// routes/tripRoutes.js
 const express = require('express');
 const router = express.Router();
 const tripController = require('../controllers/tripController');
+const galleryController = require('../controllers/galleryController');
 
-router.get('/', tripController.getAllTrips);
-router.get('/:id', tripController.getOneTrip);
-router.post('/', tripController.createTrip);
-router.delete('/:id', tripController.deleteTrip);
-router.put('/:id', tripController.updateTrip);
-router.get('/:id/gallery', tripController.getTripGallery);
-router.post('/:id/gallery', tripController.addGalleryEntry);
-router.get('/:tripId/gallery/:entryId', tripController.getGalleryEntry);
-router.put('/:tripId/gallery/:entryId', tripController.updateGalleryEntry);
-router.delete('/:tripId/gallery/:entryId', tripController.deleteGalleryEntry);
+router.route('')
+    .get(tripController.getAllTrips)
+    .post(tripController.createTrip);
+
+router.route('/:id')
+    .get(tripController.getOneTrip)
+    .put(tripController.updateTrip) // Updated this line
+    .patch(tripController.partialUpdateTrip)  // Added PATCH route
+    .delete(tripController.deleteTrip);
+
+router.route('/:id/gallery')
+    .get(galleryController.getTripGallerys)
+    .post(galleryController.addTripGallery);
+
+router.route('/:tripId/gallery/:galleryID')
+    .get(galleryController.getTripGallery)
+    .put(galleryController.updateTripGallery)
+    .patch(galleryController.partialUpdateTripGallery)  // Added PATCH route
+    .delete(galleryController.deleteTripGallery);
 
 module.exports = router;
