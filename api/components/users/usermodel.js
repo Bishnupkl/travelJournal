cd4be2326d690c8190b0ffb7737f913afd5b5bae
Bishnupkl <bishnup212@gmail.com>
2024-08-09 08:47:25 -0500

    authenticatin and user operation added

---
+++ api/components/users/usermodel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const userModel = mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: String,
+    password: {
+        type: String,
+        required: true
+    }
+});
+mongoose.model(process.env.USER_MODEL, userModel, process.env.USER_COLLECTION);
