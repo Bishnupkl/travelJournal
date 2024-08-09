9231819c50975b081bd0a55205bc5435d2e2669c
Bishnupkl <bishnup212@gmail.com>
2024-08-09 00:12:54 -0500

    workingallery controller

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 
 
-app.use(process.env.URL_TRIPS, function (req, res, next) {
+app.use(process.env.SUB_SET_ROUTE, function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
   res.header('Access-Control-Allow-Headers', 'Origin, XRequested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
