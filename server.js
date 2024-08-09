d56f2e49b8f4b8c624bb61c6cabb48cd95849415
Bishnupkl <bishnup212@gmail.com>
2024-08-08 22:56:41 -0500

    fixed problem

--- server.js
+++ server.js
@@ -5,23 +5,8 @@ const router = require('./api/routes');
 require('dotenv').config();
 
 
-<<<<<<< HEAD
-app.use(process.env.SUB_SET_ROUTE, function (req, res, next) {
-  res.header(process.env.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, process.env.ACCESS_CONTROL_ALLOW_ORIGIN_WEBSITES);
-  res.header('Access-Control-Allow-Headers', 'Origin, XRequested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
 
 
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-
-app.use(process.env.SUB_SET_ROUTE, router);
-
-=======
 app.use(process.env.URL_TRIPS, function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
   res.header('Access-Control-Allow-Headers', 'Origin, XRequested-With, Content-Type, Accept');
@@ -30,8 +15,15 @@ app.use(process.env.URL_TRIPS, function (req, res, next) {
 });
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(process.env.SUB_SET_ROUTE, router);
+
+
+app.use(express.json());
+
 app.use(process.env.URL_TRIPS, tripRoutes);
->>>>>>> 87e9e5b9b14682e235807dd799f2a64372536424
 const PORT = process.env.PORT || 3000;
 const SERVER_MSG = process.env.SERVER_MSG || "Server is running on port";
 app.listen(PORT, () => {
